e076e9da0ef62e7e6d9e71a2b0026cf52781d062
Kevin Traini <kevin@ktraini.com>
2020-09-26 10:08:38 +0200

    Add homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,23 +4,31 @@ namespace Controller;
 
 use Fork\Annotations\Route;
 use Fork\Controller\AbstractController;
-use Fork\Request\Session;
-use Fork\Response\RedirectResponse;
-use YamlEditor\YamlArray;
+use Fork\Database\Query\PreparedQuery;
 
 class HomeController extends AbstractController
 {
     /**
      * HomeController constructor.
      */
-    public function __construct() {}
-
+    public function __construct()
+    {
+    }
 
     /**
      * @Route(route="/", name="home")
      */
     public function homepage()
     {
-        return $this->render('home/homepage.html.twig');
+        $query = new PreparedQuery('SELECT title,subtitle,updatedAt FROM article ORDER BY updatedAt DESC LIMIT 5');
+        $articles = $query->getResult();
+
+        $query = new PreparedQuery('SELECT name,description,updatedAt FROM project ORDER BY updatedAt DESC LIMIT 4');
+        $projets = $query->getResult();
+
+        return $this->render('home/homepage.html.twig', [
+            'articles' => $articles,
+            'projects' => $projets
+        ]);
     }
 }
